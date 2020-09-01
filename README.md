1ad663a8dfef06c7af0cedc36534358eaca1f058
Larraine Gonzales <larrainegonzales@hotmail.com>
2020-08-31 20:35:17 -0500

    currently working code with readme ready to turn in

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # TeamBuilder-HW-08
-Team building app
+
+This is an easy to use Team Builder generator that keeps your team organized.
+
+## GIF Usage
+![gif1](https://user-images.githubusercontent.com/68169895/91782794-c2593e00-ebc3-11ea-9dc1-4cd4a770235b.gif)
+![gif2](https://user-images.githubusercontent.com/68169895/91782958-43b0d080-ebc4-11ea-836e-05f777ef7bf7.gif)
+
+## screenshots
+Sample snapshot
+![alt text](https://user-images.githubusercontent.com/68169895/91783008-5fb47200-ebc4-11ea-8d59-0b9791ef9a4d.png)
+
+## How to use
+This is a Team Builder that takes your input of your teams information and organizes all of the employees information.
+
+## Dependencies
+Jest
+<br>
+Inquirer
+
+## Testing
+To run test: in the command line once in necessary working directory input "npm test"
+<br>
+![alt text](https://user-images.githubusercontent.com/68169895/91783191-fe40d300-ebc4-11ea-9330-de3acc7dc4f3.png)
+
+## License & Copyright
+© Larraine Gonzales, Rice Coding Bootcamp
---
+++ app.js
@@ -0,0 +1,175 @@
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const inquirer = require("inquirer");
+const path = require("path");
+const fs = require("fs");
+
+const outputPath = path.resolve(__dirname, "output", "team.html");
+
+const render = require("./lib/htmlRenderer");
+
+const team = [];
+
+class teamBuilder {
+    confirmManager() {
+        inquirer
+            .prompt([
+                {
+                    type: "confirm"
+                    , message: "Are you the team manager?"
+                    , name: "managerConfirm"
+                }
+            ]).then(res => {
+                if (res.managerConfirm) {
+                    this.createManager()
+                } else {
+                    console.log("You need to be a manager to create a team!")
+                    process.exit(0);
+                }
+            })
+    };
+
+    createManager() {
+        inquirer
+            .prompt([
+                {
+                   type: "input"
+                   , message: "What is your name?"
+                   , name: "managerName"
+                },
+                {
+                   type: "input"
+                   , message: "What is your ID?"
+                   , name: "managerID"
+                },
+                {
+                   type: "input"
+                   , message: "What is your office phone?"
+                   , name: "managerPhone"
+                },
+                {
+                   type: "input"
+                   , message: "What is your office email?"
+                   , name: "managerEmail"
+                },
+            ]).then(res => {
+                const manager = new Manager(res.managerName, res.managerID, res.managerEmail, res.managerPhone);
+                team.push(manager);
+                this.createTeam();
+            });
+    };
+
+    createTeam() {
+        inquirer
+            .prompt([
+                {
+                    type: "list"
+                    , message: "What teams members would you like to create?"
+                    , choices: ["Engineer", "Intern", "I'm done Adding Team Members"]
+                    , name: "employeeType"
+                }
+            ]).then(res => {
+                if (res.employeeType ==="Engineer"){
+                    this.createEngineer();
+                } else if (res.employeeType === "Intern") {
+                    this.createIntern();
+                } else {
+                    fs.writeFile(outputPath, render(team), err => {
+                        if (err) throw err;
+                        console.log("team.html document generated!")
+                    })
+
+                }
+            });
+    };
+
+    createEngineer() {
+        inquirer
+            .prompt([
+                {
+                    type: "input"
+                    , message: "What is your engineer's name?"
+                    , name: "engineerName"
+                },
+                {
+                    type: "input"
+                    , message: "What is the engineer's ID?"
+                    , name: "engineerID"
+                },
+                {
+                    type: "input"
+                    , message: "What is the engineer's gitlab username?"
+                    , name: "engineerPhone"
+                },
+                {
+                    type: "input"
+                    , message: "What is the engineer's office email?"
+                    , name: "engineerEmail"
+                },
+            ]).then(res => {
+                const engineer = new Engineer(res.engineerName, res.engineerID, res.engineerEmail, res.engineerPhone);
+                team.push(engineer);
+                this.createTeam();
+            });
+    };
+
+    createIntern() {
+        inquirer
+            .prompt([
+                {
+                    type: "input"
+                    , message: "What is the inter's name?"
+                    , name: "internName"
+                },
+                {
+                    type: "input"
+                    , message: "What is the inter's ID?"
+                    , name: "internID"
+                },
+                {
+                    type: "input"
+                    , message: "What is the inter's school?"
+                    , name: "internSchool"
+                },
+                {
+                    type: "input"
+                    , message: "What is the inter's office email?"
+                    , name: "internEmail"
+                },
+            ]).then(res => {
+                const intern = new Intern(res.internName, res.internID, res.internEmail, res.internSchool);
+                team.push(intern);
+                this.createTeam();
+            });
+    }
+
+
+}
+
+const appStart = new teamBuilder();
+
+appStart.confirmManager();
+
+// Write code to use inquirer to gather information about the development team members,
+// and to create objects for each team member (using the correct classes as blueprints!)
+
+// After the user has input all employees desired, call the `render` function (required
+// above) and pass in an array containing all employee objects; the `render` function will
+// generate and return a block of HTML including templated divs for each employee!
+
+// After you have your html, you're now ready to create an HTML file using the HTML
+// returned from the `render` function. Now write it to a file named `team.html` in the
+// `output` folder. You can use the variable `outputPath` above target this location.
+// Hint: you may need to check if the `output` folder exists and create it if it
+// does not.
+
+// HINT: each employee type (manager, engineer, or intern) has slightly different
+// information; write your code to ask different questions via inquirer depending on
+// employee type.
+
+// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
+// and Intern classes should all extend from a class named Employee; see the directions
+// for further information. Be sure to test out each class and verify it generates an
+// object with the correct structure and methods. This structure will be crucial in order
+// for the provided `render` function to work! ```
